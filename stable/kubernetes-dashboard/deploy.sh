45f57eb3501c8dc552520e064cd1ac5965283295
Ying Huang <huangyingw@gmail.com>
2020-06-27 13:48:54 -0400

    n

--- stable/kubernetes-dashboard/deploy.sh
+++ stable/kubernetes-dashboard/deploy.sh
@@ -3,5 +3,9 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-helm uninstall wordpress
-helm install wordpress ./
+helm uninstall kubernetes-dashboard
+helm install kubernetes-dashboard ./
+
+export NODE_PORT=$(kubectl get -n default -o jsonpath="{.spec.ports[0].nodePort}" services kubernetes-dashboard)
+export NODE_IP=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}")
+echo https://$NODE_IP:$NODE_PORT/
