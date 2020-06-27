27dd1568d6bf2a694eb968c4ef4b6ee008bd144b
Ying Huang <huangyingw@gmail.com>
2020-06-27 18:49:51 -0400

    n

--- stable/kubernetes-dashboard/deploy.sh
+++ stable/kubernetes-dashboard/deploy.sh
@@ -4,8 +4,8 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 helm uninstall kubernetes-dashboard
-helm install kubernetes-dashboard ./
+helm install kubernetes-dashboard ./ --namespace kube-system
 
-export NODE_PORT=$(kubectl get -n default -o jsonpath="{.spec.ports[0].nodePort}" services kubernetes-dashboard)
+export NODE_PORT=$(kubectl get -n kube-system -o jsonpath="{.spec.ports[0].nodePort}" services kubernetes-dashboard)
 export NODE_IP=$(kubectl get nodes -o jsonpath="{.items[0].status.addresses[0].address}")
 echo https://$NODE_IP:$NODE_PORT/
